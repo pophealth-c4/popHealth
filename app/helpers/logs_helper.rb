e8423f57f8a22f3bcbaa44e3f3100dfbb2d81130
Luke Rasmussen <luke.rasmussen@gmail.com>
2016-12-21 09:08:30 -0600

    Updated log page for filtering/sorting

--- app/helpers/logs_helper.rb
+++ app/helpers/logs_helper.rb
@@ -2,6 +2,7 @@ module LogsHelper
   def time_range_params_plus(url_params_hash)
     url_params_hash[:log_start_date] = params[:log_start_date] if params[:log_start_date]
     url_params_hash[:log_end_date] = params[:log_end_date] if params[:log_end_date]
+    url_params_hash[:order] = (@sort_order== :asc ? "desc" : "asc")
     url_params_hash
   end
 
